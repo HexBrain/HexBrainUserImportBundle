326a50c57e3971c80aa200bbf787d26bff6f9535
Vlad Kupratyi <vlad@hexbrain.com>
2017-06-08 12:40:43 +0300

    update to 1.10 oro platform

--- ImportExport/Strategy/UserAddOrReplaceStrategy.php
+++ ImportExport/Strategy/UserAddOrReplaceStrategy.php
@@ -14,18 +14,43 @@ class UserAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
     protected $encoderFactory;
 
     /**
+     * @param EventDispatcherInterface $eventDispatcher
      * @param ImportStrategyHelper $helper
      * @param FieldHelper $fieldHelper
+     * @param DatabaseHelper $databaseHelper
+     * @param ChainEntityClassNameProvider $chainEntityClassNameProvider
+     * @param TranslatorInterface $translator
+     * @param NewEntitiesHelper $newEntitiesHelper
+     * @param DoctrineHelper $doctrineHelper
      * @param ConfigManager $configManager
      * @param EncoderFactoryInterface $encoderFactory
      */
-    public function __construct(ImportStrategyHelper $helper, FieldHelper $fieldHelper, $databaseHelper, ConfigManager $configManager, EncoderFactoryInterface $encoderFactory)
+    public function __construct(
+        EventDispatcherInterface $eventDispatcher,
+        ImportStrategyHelper $helper,
+        FieldHelper $fieldHelper,
+        DatabaseHelper $databaseHelper,
+        ChainEntityClassNameProvider $chainEntityClassNameProvider,
+        TranslatorInterface $translator,
+        NewEntitiesHelper $newEntitiesHelper,
+        DoctrineHelper $doctrineHelper,
+        ConfigManager $configManager,
+        EncoderFactoryInterface $encoderFactory
+    )
     {
-        $this->strategyHelper = $helper;
-        $this->fieldHelper = $fieldHelper;
+        parent::__construct(
+            $eventDispatcher,
+            $helper,
+            $fieldHelper,
+            $databaseHelper,
+            $chainEntityClassNameProvider,
+            $translator,
+            $newEntitiesHelper,
+            $doctrineHelper
+        );
+
         $this->configManager = $configManager;
         $this->encoderFactory = $encoderFactory;
-        $this->databaseHelper = $databaseHelper;
     }
 
     /**
