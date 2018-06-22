b98098ea6a788985b7d14cacd947f02e5bdae497
maks <max@hexbrain.com>
2018-06-22 17:55:39 +0300

    updated modul

--- ImportExport/Strategy/UserAddOrReplaceStrategy.php
+++ ImportExport/Strategy/UserAddOrReplaceStrategy.php
@@ -4,10 +4,17 @@ namespace HexBrain\Bundle\UserImportBundle\ImportExport\Strategy;
 
 use Oro\Bundle\ImportExportBundle\Strategy\Import\ConfigurableAddOrReplaceStrategy;
 use Oro\Bundle\ImportExportBundle\Strategy\Import\ImportStrategyHelper;
-use Oro\Bundle\ImportExportBundle\Field\FieldHelper;
+use Oro\Bundle\ImportExportBundle\Field\DatabaseHelper;
+use Oro\Bundle\EntityBundle\Helper\FieldHelper;
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Oro\Bundle\EntityBundle\Provider\ChainEntityClassNameProvider;
+use Symfony\Component\Translation\TranslatorInterface;
+use Oro\Bundle\ImportExportBundle\Strategy\Import\NewEntitiesHelper;
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\SecurityBundle\Owner\OwnerChecker;
 class UserAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
 {
     protected $configManager;
@@ -34,10 +41,10 @@ class UserAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
         TranslatorInterface $translator,
         NewEntitiesHelper $newEntitiesHelper,
         DoctrineHelper $doctrineHelper,
+        OwnerChecker $ownerChecker,
         ConfigManager $configManager,
         EncoderFactoryInterface $encoderFactory
-    )
-    {
+    ){
         parent::__construct(
             $eventDispatcher,
             $helper,
@@ -46,11 +53,11 @@ class UserAddOrReplaceStrategy extends ConfigurableAddOrReplaceStrategy
             $chainEntityClassNameProvider,
             $translator,
             $newEntitiesHelper,
-            $doctrineHelper
+            $doctrineHelper,
+            $ownerChecker
         );
-
-        $this->configManager = $configManager;
         $this->encoderFactory = $encoderFactory;
+        $this->configManager = $configManager;
     }
 
     /**
